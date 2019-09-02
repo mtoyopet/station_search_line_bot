57123777511f7d84221c0bf7f0d6c11682bcf128
mtoyopet <toyopet-momoko@hotmail.co.jp>
2019-09-02 20:58:38 +0900

    update

--- app.rb
+++ app.rb
@@ -22,7 +22,6 @@ post '/callback' do
   end
 
   events = client.parse_events_from(body)
-
   events.each do |event|
     case event
     when Line::Bot::Event::Message
@@ -34,13 +33,14 @@ post '/callback' do
         }
         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Sticker
-        package_id = event.message['packageId']
-        sticker_id = event.message['stickerId']
+        # package_id = event.message.packageId
+        # sticker_id = event.message.stickerId
 
         message = {
-          type: 'sticker',
-          packageId: package_id,
-          stickerId: sticker_id
+          type: 'text',
+          text: event.message
+          # packageId: "11537",
+          # stickerId: "52002734"
           }
 
         client.reply_message(event['replyToken'], message)
