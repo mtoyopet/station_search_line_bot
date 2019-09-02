7434ae3832b564af2f926b4eb42a462b3e25afdc
mtoyopet <toyopet-momoko@hotmail.co.jp>
2019-09-02 20:22:55 +0900

    update

--- app.rb
+++ app.rb
@@ -32,6 +32,12 @@ post '/callback' do
           text: event.message['text']
         }
         client.reply_message(event['replyToken'], message)
+      when Line::Bot::Event::MessageType::Sticker
+        message = {
+          type: 'text',
+          text: 'こら！！！！！'
+        }
+        client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
