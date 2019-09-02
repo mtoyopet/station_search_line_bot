8edb06292b903110ba52d52d55fdbb456ba24e74
mtoyopet <toyopet-momoko@hotmail.co.jp>
2019-09-03 00:01:22 +0900

    update

--- lib/find_station.rb
+++ lib/find_station.rb
@@ -15,7 +15,7 @@ def station_api(longitude, latitude)
   res = Net::HTTP.get_response(uri)
   stations = JSON.parse(res.body)["response"]["station"]
 
-  result = stations.map { |station| "#{station['line']} #{station['name'}駅 (#{station['distance']}メートル)"}.join("\n")
+  result = stations.map { |station| "#{station['line']} #{station['name']}駅 (#{station['distance']}メートル)" }.join("\n")
 
-  return result
+  result
 end
